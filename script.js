c270bc43f1a1f8931d577750f69afb6893f46326
arthurgael123321-source <arthurgael123321@gmail.com>
2026-08-03 18:33:25 -0300

    Feat: 90% da parte das origems, criação da pagina agentes, organizaçao do header, pequena melhora na pagina home e inicio da criação da pagina classes

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 
 let paginaAtributos = document.getElementById("pagina-atributos");
+
 let paginaOrigem = document.getElementById("pagina-origem");
 
+let detalhesCards = document.querySelectorAll("div.card-origem");
+
 let botaoAtributos = document.getElementById("botao-atributos");
+
+let botaoClasses = document.getElementById("botao-classes");
+
+let paginaClasses = document.getElementById("pagina-classes");
+
+let botaoOrigem = document.getElementById("botao-origem");
+
+
 
 botaoAtributos.addEventListener("click", function (){
 
     paginaOrigem.style.display = "none"
     paginaAtributos.style.display = "flex"
+    paginaClasses.style.display = "none"    
 
 });
 
-let botaoOrigem = document.getElementById("botao-origem");
 
 botaoOrigem.addEventListener("click", function (){
 
     paginaAtributos.style.display = "none"
     paginaOrigem.style.display = "flex"
+    paginaClasses.style.display = "none"
 
 });
+
+botaoClasses.addEventListener("click", function() {
+    paginaClasses.style.display = "flex"
+    paginaAtributos.style.display = "none"
+    paginaOrigem.style.display = "none"
+});
+
+
+detalhesCards.forEach(function(card){
+    card.addEventListener("click", function(){
+        card.classList.toggle("aberto")
+
+    })
+});
+    
+
+
 
 
